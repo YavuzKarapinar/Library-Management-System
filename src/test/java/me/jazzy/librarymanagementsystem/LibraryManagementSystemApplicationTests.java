e60cda060abfa12ad3ccb30dc09ca0c0ca788e09
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-02-23 19:20:35 +0300

    init

---
+++ src/test/java/me/jazzy/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jazzy.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
