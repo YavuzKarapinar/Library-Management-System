cd7a5fadf9e3edc2dcef92c42edd600f8a8850bd
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-02-27 22:49:49 +0300

    fix: Param bug fixed

--- src/main/java/me/jazzy/librarymanagementsystem/repository/CategoryRepository.java
+++ src/main/java/me/jazzy/librarymanagementsystem/repository/CategoryRepository.java
@@ -3,11 +3,12 @@ package me.jazzy.librarymanagementsystem.repository;
 import me.jazzy.librarymanagementsystem.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Query("select distinct c.id, c.categoryName, c.categoryDefinition from Category c where c.categoryName =: name")
-    Optional<Category> findByCategoryName(String name);
+    @Query("select distinct c from Category c where c.categoryName = :name")
+    Optional<Category> findByCategoryName(@Param("name") String name);
 }
